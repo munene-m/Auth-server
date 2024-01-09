a4e248de6a429213067e992e69a424d6dd2d9d21
munene-m <munenenjue18@gmail.com>
2024-01-09 23:38:11 +0300

    chore: create register, login and profile functions

---
+++ src/controllers/authController.js
@@ -0,0 +1,101 @@
+const db = require("../models");
+const User = db.users;
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { Op } = require("sequelize");
+const {
+  validateRegistration,
+  validateLogin,
+} = require("../helpers/validation.js");
+
+const registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const validationError = validateRegistration(username, email, password);
+  if (validationError) {
+    return res.status(400).json({ message: validationError.message });
+  }
+
+  try {
+    const existingUser = await User.findOne({
+      where: {
+        [Op.or]: [{ username }, { email }],
+      },
+    });
+
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Username or email already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const isAdmin = email === process.env.ADMIN_EMAIL;
+
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+      role: isAdmin ? "admin" : "user",
+    });
+    const token = generateToken(newUser);
+
+    res
+      .status(201)
+      .json({ message: "User registered successfully", user: newUser, token });
+  } catch (error) {
+    console.error("Error during user registration:", error);
+    res.status(400).json({ message: "An error occurred" });
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const validationError = validateLogin(email, password);
+  if (validationError) {
+    return res.status(400).json({ message: validationError.message });
+  }
+  try {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const token = generateToken(user);
+
+    res.status(200).json({ user, token });
+  } catch (error) {
+    console.error("Error during user registration:", error);
+    res.status(400).json({ message: "An error occurred", error });
+  }
+};
+
+const getProfile = async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const user = await User.findByPk(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Error finding user:", error);
+    res
+      .status(400)
+      .json({ message: "An error occurred when fetching user", error });
+  }
+};
+
+const generateToken = (user) => {
+  return jwt.sign({ user }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
+
+module.exports = { registerUser, loginUser, getProfile };
